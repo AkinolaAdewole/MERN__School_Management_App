66cd5b2a0722ed0926ffd5d1f0640f114837bcd6
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-15 23:26:54 +0100

    School Management App

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,11 @@ const connectDB = async()=>{
     try {
         await mongoose.connect(process.env.Mongo_URI, {
             dbName: "Sch-Management",
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true
         })
+
+        console.log("MongoDB is connected");
     } catch (error) {
         console.error(error);
     }
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv';
 dotenv.config()
 
-const port = process.env.port || 3200
+const port = process.env.port || 3100
+
+import connectDB from './config/db.js';
+connectDB()
 
 const app = express()
 app.use(express.json())
