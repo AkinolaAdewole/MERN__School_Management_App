43c77db9fd279b3f412dffdf1baef9784ef216d2
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-15 17:42:17 +0100

    School Management App

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ dotenv.config()
 const port = process.env.port || 3200
 
 const app = express()
+
+app.get('/api', (req,res)=>{
+    res.status(200).json({ message: "Server is working"});
+});
 app.listen(port, ()=>{
     console.log(`server running on port ${port}`);
 })
