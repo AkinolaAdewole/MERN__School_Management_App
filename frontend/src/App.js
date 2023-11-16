fea6cb1b98184d23634d73e7607f75fc8dde88d3
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-16 13:04:57 +0100

    School Management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route,} from 'react-router-dom'
+import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
   return (
-      <></>
+      <>
+        <Router>
+          <Routes>
+            <Route path='/' element={<LandingPage />}/>
+          </Routes>
+        </Router>
+      </>
   );
 }
 
