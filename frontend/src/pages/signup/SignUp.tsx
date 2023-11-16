7c3d15f9387f908163febe63b16c79b4c3313506
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-16 16:50:57 +0100

    School Management

--- frontend/src/pages/signup/SignUp.tsx
+++ frontend/src/pages/signup/SignUp.tsx
@@ -21,9 +21,27 @@ const SignUp = () => {
         <div className='container-fluid'>
           <div className='row'>
             <div className='col-6 shadow-sm mx-auto'>
+
               <form action="">
-                <label htmlFor="">Firstname</label>
-                <input type="text" name='firstname' />
+
+                <label htmlFor="" className='form-control my-2'>Firstname</label>
+                <input type="text" name='firstname' 
+                  className='form-control my-2' 
+                  value={formData.firstname} />
+
+                <label htmlFor="">Lastname</label>
+                <input type="text" name='lastname' 
+                  className='form-control my-2'
+                  value={formData.lastname}/>
+
+                  <label htmlFor="">Email</label>
+                  <input type="text" name='email' 
+                    className='form-control my-2'
+                    value={formData.email}/>
+
+                  <label htmlFor="">Password</label>
+                  <input type="password" name='password'
+                    className='form-control my-2'/>
               </form>
             </div>
           </div>
