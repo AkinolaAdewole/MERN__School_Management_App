bdced8278de6f2c4717715a2e860287650f83c74
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-16 13:56:59 +0100

    School Management

---
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+
+interface SignUpFormData {
+  firstname: String;
+  lastname: String;
+  email: String;
+  password: String;
+}
+const SignUp = () => {
+
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignUp
